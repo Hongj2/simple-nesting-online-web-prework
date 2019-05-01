1b57388bc7596d8ad9e8b4ff53560b69c51963ef
Jessica Hong <jessicahong.24@gmail.com>
2019-05-01 02:51:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     return programmer_hash [:alan_kay].values_at(:known_for).join(" ")
+      programmer_hash[:alan_kay][:known_for]
   
 end
 
@@ -107,6 +107,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+
    #  programmer_hash [:alan_kay].values_at(:known_for).join == [alans_new_info
    #return programmer_hash [:alan_kay].values_at(:known_for)
    
@@ -115,6 +116,12 @@ def changing_alan
       programmer_hash [:alan_kay][:known_for] = alans_new_info
     
     return programmer_hash 
+
+     alans_new_info = "GUI"
+     programmer_hash [:alan_kay].values_at(:known_for) 
+ 
+     
+
 end
 
 def adding_to_dennis
